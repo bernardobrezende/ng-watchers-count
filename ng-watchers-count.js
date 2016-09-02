98d647008870fd9d415aff19a43a6a35450bb227
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-09-02 14:57:59 -0300

    feat(watchers-ribbon): add initial implementation.

---
+++ ng-watchers-count.js
@@ -0,0 +1,42 @@
+'use strict';
+var watchersCount = function () { 
+  // credits to: http://stackoverflow.com/a/18526757/5194966
+  var root = angular.element(document.getElementsByTagName('body'));
+  var watchers = [];
+  var f = function (element) {
+    angular.forEach(['$scope', '$isolateScope'], function (scopeProperty) { 
+      if (element.data() && element.data().hasOwnProperty(scopeProperty)) {
+        angular.forEach(element.data()[scopeProperty].$$watchers, function (watcher) {
+          watchers.push(watcher);
+        });
+      }
+    });
+
+    angular.forEach(element.children(), function (childElement) {
+      f(angular.element(childElement));
+    });
+  };
+
+  f(root);
+
+  // Remove duplicate watchers
+  var watchersWithoutDuplicates = [];
+  angular.forEach(watchers, function(item) {
+    if(watchersWithoutDuplicates.indexOf(item) < 0) {
+      watchersWithoutDuplicates.push(item);
+    }
+  });
+
+  function createOverlay(element, amount){
+    amount = 'Angular watchers: ' + amount;
+    var over = angular.element('<div style="background-color: red; color: white; position: fixed; right: 20; bottom: 10;">' + amount + '</div>')
+    over.width = element.width;
+    over.height = element.height;
+    var overlayMain = angular.element('<div/>'); 
+    overlayMain.css({'z-index': 9999});
+    overlayMain.append(over);
+    angular.element(document.body).append(overlayMain);
+  }
+  createOverlay(root[0], watchersWithoutDuplicates.length);
+};
+setInterval(watchersCount, 1500);
